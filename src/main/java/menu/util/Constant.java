c9cb2075a0c5769adb43a65b8af8a873d0b49a76
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-12-15 15:57:27 +0900

    feat(Util) : 각종 상수 처리하는 Constant 추가

---
+++ src/main/java/menu/util/Constant.java
@@ -0,0 +1,8 @@
+package menu.util;
+
+public class Constant {
+    public static final Integer COACH_ROSTER_MIN_SIZE = 2;
+    public static final Integer COACH_ROSTER_MAX_SIZE = 5;
+    public static final Integer COACH_NAME_MIN_LENGTH = 2;
+    public static final Integer COACH_NAME_MAX_LENGTH = 4;
+}
