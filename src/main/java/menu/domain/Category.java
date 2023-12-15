fe872be5cf853cf7c678efc42bca02e24634ee42
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-12-15 17:32:26 +0900

    feat(Category) : getName , Index로 카테고리 가져오게 구현

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,14 +1,32 @@
 package menu.domain;
 
 public enum Category {
-    KOREAN("한식"),
-    JAPAN("일식"),
-    CHINA("중식"),
-    ASIAN("아시안"),
-    WESTERN("양식");
+    JAPAN("일식", 1),
+    KOREAN("한식", 2),
+
+    CHINA("중식", 3),
+    ASIAN("아시안", 4),
+    WESTERN("양식", 5);
     String name;
-    private Category(String name){
+    Integer index;
+
+    public String getName() {
+        return name;
+    }
+
+    private Category(String name, Integer index) {
         this.name = name;
+        this.index = index;
+    }
+
+    public static Category getCategoryFromIndex(Integer index) {
+        for (Category category : Category.values()) {
+            if (category.index.equals(index)) {
+                return category;
+            }
+        }
+        // 나올 경우가 없으 므로 , null 처리
+        return null;
     }
 
 }
